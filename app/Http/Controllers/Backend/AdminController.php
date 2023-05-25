759cee08f94232e1460a749b8fc3d5d0411dbbdd
mahbub095 <mahbub.095@gmail.com>
2023-05-25 11:42:42 +0600

    Admin login complete

--- app/Http/Controllers/Backend/AdminController.php
+++ app/Http/Controllers/Backend/AdminController.php
@@ -9,6 +9,11 @@ class AdminController extends Controller
 {
     public function dashboard()
     {
-       return view('admin.dashboard');
+        return view('admin.dashboard');
+    }
+
+    public function login()
+    {
+        return view('admin.auth.login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Backend\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,3 +31,4 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__ . '/auth.php';
 
+Route::get('admin/login', [AdminController::class, 'login'])->name('admin.login');
