034d363d756d55e43e28dc1759f8c955f64dee23
mahbub095 <mahbub.095@gmail.com>
2023-06-06 13:37:53 +0600

    Vendor image gallery

--- routes/vendor.php
+++ routes/vendor.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\Backend\VendorProductController;
 use App\Http\Controllers\Backend\VendorProfileController;
 use App\Http\Controllers\Backend\VendorShopProfileController;
+use App\Http\Controllers\Backend\VendorProductImageGalleryController;
 use Illuminate\Support\Facades\Route;
 
 //Vendor Route
@@ -22,3 +23,6 @@ Route::get('product/get-subcategories', [VendorProductController::class, 'getSub
 Route::get('product/get-child-categories', [VendorProductController::class, 'getChildCategories'])->name('product.get-child-categories');
 Route::put('product/change-status', [VendorProductController::class, 'changeStatus'])->name('product.change-status');
 Route::resource('products', VendorProductController::class);
+
+/** Products image gallery route */
+Route::resource('products-image-gallery', VendorProductImageGalleryController::class);
