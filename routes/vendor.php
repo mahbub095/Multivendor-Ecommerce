90d1379d2ecdc8b4fced0a674d802e4d25e270ec
mahbub095 <mahbub.095@gmail.com>
2023-06-06 15:54:01 +0600

    VendorProductVariantController

--- routes/vendor.php
+++ routes/vendor.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Backend\VendorProductController;
 use App\Http\Controllers\Backend\VendorProfileController;
 use App\Http\Controllers\Backend\VendorShopProfileController;
 use App\Http\Controllers\Backend\VendorProductImageGalleryController;
+use App\Http\Controllers\Backend\VendorProductVariantController;
 use Illuminate\Support\Facades\Route;
 
 //Vendor Route
@@ -26,3 +27,7 @@ Route::resource('products', VendorProductController::class);
 
 /** Products image gallery route */
 Route::resource('products-image-gallery', VendorProductImageGalleryController::class);
+
+/** Products variant route */
+Route::put('products-variant/change-status', [VendorProductVariantController::class, 'changeStatus'])->name('products-variant.change-status');
+Route::resource('products-variant', VendorProductVariantController::class);
