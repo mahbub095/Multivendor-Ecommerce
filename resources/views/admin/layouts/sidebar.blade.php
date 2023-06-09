da87aae000b4b13ab92c159bce8641748d3962ec
mahbub095 <mahbub.095@gmail.com>
2023-06-09 16:10:11 +0600

    FlashSale

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -82,7 +82,7 @@
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
                     <span>Ecommerce</span></a>
                 <ul class="dropdown-menu">
-                    <li class=""><a class="nav-link" href="">Flash Sale</a></li>
+                    <li class=""><a class="nav-link" href="{{ route('admin.flash-sale.index') }}">Flash Sale</a></li>
                     <li class=""><a class="nav-link" href="">Coupons</a></li>
                     <li class=""><a class="nav-link" href="">Shipping Rule</a></li>
                     <li class=""><a class="nav-link" href="{{ route('admin.vendor-profile.index') }}">Vendor Profile</a>
